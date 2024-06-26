c490a9755273b9d73b421f87b24e87d02183b9b6
vishnu kumar sharma <144815213+vishnuksharma78@users.noreply.github.com>
2024-06-26 14:19:29 +0000

    string

--- basic/04.js
+++ basic/04.js
@@ -4,3 +4,24 @@
 // Heap memory - > heap memory is used in non-primitive data types like array,object,functions .
 //   in heap the values are passed by reference means changes in origional values 
 
+const name = "vishnu"
+const repo = 20 
+// console.log(`Hello my name is ${name} and i have ${repo}`);
+
+const game = new String("  shiv sha ram   ")
+// console.log(game.charAt(2));
+// console.log(game.indexOf('s'));
+// console.log(game.lastIndexOf('s'));
+// console.log(game.includes('sh'));
+// console.log(game.split(' '));
+// console.log(game.slice(-5,11));
+// console.log(game.substring(2,6));
+// console.log(game.replace('ram','sita'));
+// console.log(game.trim());
+// console.log(game.toUpperCase());
+// console.log(game[0]);
+// to travas string 
+// for(let i = 0 ; i<game.length ; i++){
+//     console.table(game[i] + " ");
+// }
+
