f3f760f5d3a167227f246966098842e73f4817dc
vishnu kumar sharma <144815213+vishnuksharma78@users.noreply.github.com>
2024-06-26 10:27:53 +0000

    stack and heap

---
+++ basic/04.js
@@ -0,0 +1,6 @@
+// stack and heap 
+//  stack memory -> stack is used to store primitive data types . 
+// in stack values are passes by call by value means pass a copy of value not original value
+// Heap memory - > heap memory is used in non-primitive data types like array,object,functions .
+//   in heap the values are passed by reference means changes in origional values 
+
